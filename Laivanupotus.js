ca8e4de9e0f96e603cf398f9727781239840037e
atlol <114558368+atlol@users.noreply.github.com>
2023-01-17 16:35:44 +0200

    Initial import

---
+++ Laivanupotus.js
@@ -0,0 +1,21 @@
+var laivanupotus = {};
+
+laivanupotus.table = function(){
+    var tArea = document.getElementById("tableArea");
+    var teksti;
+    var t = "";
+    for(i=0; i<10; i++) {
+        t += "<tr>";
+        for(j=0; j<10; j++) {
+            teksti = '<td> <button onclick="laivanupotus.painallus('+i+','+j+')"';
+            teksti += "id="+i +"."+j+" class="+"button1"+"> "+i+","+j+" </button> </td>"; 
+            t += teksti;
+        }
+        t += "</tr>";
+    }
+    tArea.innerHTML = t;
+}
+
+laivanupotus.painallus = function (x, y) {
+    console.log(x,y)
+}
