8c09412b4aeb14608814fc544ec4bbce9c08c398
atlol <114558368+atlol@users.noreply.github.com>
2023-01-17 18:46:43 +0200

    laivanupotus ja vihollisenlaivat toimii

--- Laivanupotus.js
+++ Laivanupotus.js
@@ -1,7 +1,9 @@
 var laivanupotus = {};
-var omatLaivat =[];
+var omatLaivat = [];
+var vihollisenLaivat = [];
 
 laivanupotus.table = function(){
+    //Pelaajan laivat
     var tArea = document.getElementById("tableArea");
     var teksti;
     var t = "";
@@ -15,21 +17,70 @@ laivanupotus.table = function(){
         t += "</tr>";
     }
     tArea.innerHTML = t;
+
+    //Vihollisen laivat
+    var tAreaEnemy = document.getElementById("tableAreaEnemy");
+    var tekstiEnemy;
+    var tEnemy = "";
+    for(h=0; h<10; h++) {
+        tEnemy += "<tr>";
+        for(g=0; g<10; g++) {
+            tekstiEnemy = '<td> <button onclick="laivanupotus.painallus('+h+','+g+')"';
+            tekstiEnemy += "id="+h +"."+g+" class="+"button1"+"> "+h+","+g+" </button> </td>"; 
+            teksti += tekstiEnemy;
+        }
+        teksti += "</tr>";
+    }
+    tArea.innerHTML = t;
+
+    laivanupotus.vihollisenLaivat();
 }
 
 laivanupotus.painallus = function (x, y) {
+    var idNimi = String(x) + "." + String(y);
+    var nappiSijainti = document.getElementById(idNimi);
     if(omatLaivat.length<=2){
-        console.log(x,y);
-        idNimi = String(x) + "." + String(y);
-        console.log(idNimi)
-        var nappiSijainti = document.getElementById(idNimi);
         nappiSijainti.classList.add("painettu");
         omatLaivat.push(Number(idNimi));
         console.log(omatLaivat);
+        if(omatLaivat.length==3){
+            document.getElementById("pelivaihe").innerHTML = "Pelivaihe: Upota vihollisen laivat."  
+        }
     }else{
-        document.getElementById("pelivaihe").innerHTML = "Pelivaihe: Upota vihollisen laivat."
-        
+        // upotusvaihe
+        if(laivanupotus.findShipFromTable(idNimi)){
+            document.getElementById("osumaTilasto").innerHTML = "Osuit"
+            nappiSijainti.classList.add("osuma");
+
+        }else{
+            document.getElementById("osumaTilasto").innerHTML = "Et osunut"
+            nappiSijainti.classList.add("ohi");
+        }
+    
     }
-    
-    
+    laivanupotus.countships();
 }   
+laivanupotus.countships = function () {
+    document.getElementById("shipcount").innerHTML = "Sinulla laivoja: " + omatLaivat.length + ". Vihollisella laivoja: "+ vihollisenLaivat.length+"."
+}
+laivanupotus.findShipFromTable = function (idNimi){
+    for(i=0;i<vihollisenLaivat.length;i++){
+        if(idNimi == vihollisenLaivat[i]){
+            vihollisenLaivat.splice([i],1);
+            console.log(vihollisenLaivat)
+            return true;
+        }
+    }
+    return false
+}
+
+laivanupotus.vihollisenLaivat = function() {
+    var vihollisenKordinaatit = Number();
+    for(i=0;i<3;i++){
+        var randomX = Number(Math.floor(Math.random() * 9));
+        var randomY = Number(Math.floor(Math.random() * 9));
+        vihollisenKordinaatit = String(randomX) + "." + String(randomY)
+        vihollisenLaivat.push(vihollisenKordinaatit)
+        console.log(vihollisenLaivat);
+    }
+}
